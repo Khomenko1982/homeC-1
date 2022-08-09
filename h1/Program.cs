af1b8283f2df16c67c136aa2cf00942f1e7b7057
«Andrey» <khomehko@mail.ru>
2022-08-09 17:04:21 +0300

    h1

--- h1/Program.cs
+++ h1/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Задача 1. Напишите программу, которая на вход принимает два числа и проверяет, является ли первое число квадратом второго.
+//a = 25, b = 5 -> да
+//a = 2, b = 10 -> нет
+//a = 9, b = -3 -> да
+//a = -3 b = 9 -> нет
+Console.WriteLine("Проверяем, является ли первое число квадратом второго.");
+Console.Write("Введите первое число: ");
+int a = Convert.ToInt16(Console.ReadLine());
+Console.Write("Введите Второе число: ");
+int b = Convert.ToInt16(Console.ReadLine());
+//new Random().Next(1,10) ;
+
+if ((a/b)==b)
+{
+Console.WriteLine("Да, первое число является квадратом второго ");
+}
+else
+{
+  Console.WriteLine("Нет,первое число  не является квадратом второго");  
+}
