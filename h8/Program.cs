d3fc5e6d308f8288dbd1e908d4fefb12b2cb5aef
«Andrey» <khomehko@mail.ru>
2022-08-10 05:12:18 +0300

    h8

---
+++ h8/Program.cs
@@ -0,0 +1,15 @@
+﻿// Задача 8: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+
+// 5 -> 2, 4
+// 8 -> 2, 4, 6, 8
+
+Console.Write("Введите число: ");
+int N = Convert.ToInt16(Console.ReadLine());
+
+N=Math.Abs(N);
+int nach=0;
+while (N-1>nach)
+{
+Console.Write(nach=nach+2);
+Console.Write(" ");
+}
